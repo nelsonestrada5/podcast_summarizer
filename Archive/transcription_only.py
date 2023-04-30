2859941ae87c4e3cbc2670b276f395330b725821
Nelson Estrada <nelsonestrada5@gmail.com>
2023-04-29 20:43:37 -0400

    Create transcription_only.py

---
+++ Archive/transcription_only.py
@@ -0,0 +1,12 @@
+from pydub import AudioSegment
+
+podcast = AudioSegment.from_file("estoy_escribiendo_un_libro.m4a")
+
+# PyDub handles time in milliseconds
+ten_minutes = 10 * 60 * 1000
+one_minute = 10 * 60 * 1000
+
+
+first_minute = podcast[:one_minute]
+
+first_minute.export("good_morning_10.mp3", format="mp3")

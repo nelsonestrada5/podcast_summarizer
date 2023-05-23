3a314ced96c2adc2581b6224aee29ef17343e1ac
Nelson Estrada <nelsonestrada5@gmail.com>
2023-05-22 20:57:48 -0400

    Include the /summarize method

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, request, jsonify
+import urllib.request
+
+# Assume we have the transcribe_and_summarize function here
+# from transcribe_and_summarize import transcribe_and_summarize
 
 app = Flask(__name__)
 
@@ -6,6 +10,25 @@ app = Flask(__name__)
 def home():
     return "Hello, World!"
 
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    data = request.get_json()
+    podcast_url = data.get('podcast_url')
+
+    # Validate podcast_url
+    if not podcast_url:
+        return jsonify({'error': 'No podcast URL provided'}), 400
+
+    # Download the podcast file
+    # Note: This is a simplified example, in real-world applications,
+    # you'd likely want to download this to a more secure location or cloud storage.
+    podcast_file = urllib.request.urlretrieve(podcast_url, 'podcast.mp3')
+
+    # Call the function that transcribes and summarizes the podcast
+    # We'll use a placeholder here for the sake of example
+    summary = "Summary placeholder" #transcribe_and_summarize(podcast_file)
+
+    return jsonify({'summary': summary})
+
 if __name__ == "__main__":
     app.run(debug=True)
-

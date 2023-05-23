9f292eee9d69e873abca483891e7c2887a612c89
Nelson Estrada <nelsonestrada5@gmail.com>
2023-05-23 17:45:56 -0400

    Improving the app.py file with stubs. Will eventually add code to functions.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 import urllib.request
+import requests
+import shutil
 
 # Assume we have the transcribe_and_summarize function here
 # from transcribe_and_summarize import transcribe_and_summarize
@@ -12,32 +14,43 @@ def home():
 
 @app.route('/summarize', methods=['POST'])
 def summarize():
-    # Get the podcast URL from the request data
     data = request.get_json()
-    podcast_url = data.get('podcast_url')   
-    if podcast_url is None:
+
+    if not data:
+        return jsonify({'error': 'No data provided'}), 400
+
+    podcast_url = data.get('podcast_url')
+
+    if not podcast_url:
         return jsonify({'error': 'No podcast URL provided'}), 400
 
-    # Download the podcast
-    import requests
-    import shutil
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537',
+        }
+        response = requests.get(podcast_url, headers=headers, stream=True)
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537',
-    }
+        if response.status_code == 200:
+            with open('podcast.mp3', 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+            print("Podcast downloaded")
+        else:
+            return jsonify({'error': 'Failed to download podcast, status code:' + str(response.status_code)}), 400
 
-    response = requests.get(podcast_url, headers=headers, stream=True)
+        # Begin your podcast processing here, for example:
+        # transcript = transcribe_podcast('podcast.mp3')
+        # summary = summarize_transcript(transcript)
+        # Replace these lines with the actual code for transcription and summarization.
 
-    if response.status_code == 200:
-        with open('podcast.mp3', 'wb') as out_file:
-            shutil.copyfileobj(response.raw, out_file)
-        print("Podcast downloaded")
-    else:
-        return jsonify({'error': 'Failed to download podcast, status code:' + str(response.status_code)}), 400
+        transcript = "This is a placeholder transcript"
+        summary = "This is a placeholder summary"
 
-    # Call your podcast summarization function here and send the results back to the client
-    # ...
-    return jsonify({'summary': 'This is a placeholder summary'}), 200
+        return jsonify({
+            'transcript': transcript,
+            'summary': summary
+        }), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)

c8454d3cc5addc653f92810cc9938b074ced317b
Nelson Estrada <nelsonestrada5@gmail.com>
2023-05-22 22:21:23 -0400

    Add headers to the /summarize request and add error handling

--- app.py
+++ app.py
@@ -12,23 +12,32 @@ def home():
 
 @app.route('/summarize', methods=['POST'])
 def summarize():
+    # Get the podcast URL from the request data
     data = request.get_json()
-    podcast_url = data.get('podcast_url')
-
-    # Validate podcast_url
-    if not podcast_url:
+    podcast_url = data.get('podcast_url')   
+    if podcast_url is None:
         return jsonify({'error': 'No podcast URL provided'}), 400
 
-    # Download the podcast file
-    # Note: This is a simplified example, in real-world applications,
-    # you'd likely want to download this to a more secure location or cloud storage.
-    podcast_file = urllib.request.urlretrieve(podcast_url, 'podcast.mp3')
+    # Download the podcast
+    import requests
+    import shutil
 
-    # Call the function that transcribes and summarizes the podcast
-    # We'll use a placeholder here for the sake of example
-    summary = "Summary placeholder" #transcribe_and_summarize(podcast_file)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537',
+    }
 
-    return jsonify({'summary': summary})
+    response = requests.get(podcast_url, headers=headers, stream=True)
+
+    if response.status_code == 200:
+        with open('podcast.mp3', 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        print("Podcast downloaded")
+    else:
+        return jsonify({'error': 'Failed to download podcast, status code:' + str(response.status_code)}), 400
+
+    # Call your podcast summarization function here and send the results back to the client
+    # ...
+    return jsonify({'summary': 'This is a placeholder summary'}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)

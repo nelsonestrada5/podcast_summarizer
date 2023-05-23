341a5311bbda9ee90da07e045fde899abb3fc6de
Nelson Estrada <nelsonestrada5@gmail.com>
2023-05-23 18:30:22 -0400

    More updated to app.py

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 from flask import Flask, request, jsonify
-import urllib.request
-import requests
-import shutil
-
-# Assume we have the transcribe_and_summarize function here
-# from transcribe_and_summarize import transcribe_and_summarize
+from transcribe_and_summarize import summarize_local_podcast
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return app.send_static_file('index.html')
 
 @app.route('/summarize', methods=['POST'])
 def summarize():
@@ -20,35 +15,14 @@ def summarize():
         return jsonify({'error': 'No data provided'}), 400
 
     podcast_url = data.get('podcast_url')
-
+    test_run = data.get('test_run', False)
     if not podcast_url:
         return jsonify({'error': 'No podcast URL provided'}), 400
 
+    # Download and summarize the podcast
     try:
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537',
-        }
-        response = requests.get(podcast_url, headers=headers, stream=True)
-
-        if response.status_code == 200:
-            with open('podcast.mp3', 'wb') as out_file:
-                shutil.copyfileobj(response.raw, out_file)
-            print("Podcast downloaded")
-        else:
-            return jsonify({'error': 'Failed to download podcast, status code:' + str(response.status_code)}), 400
-
-        # Begin your podcast processing here, for example:
-        # transcript = transcribe_podcast('podcast.mp3')
-        # summary = summarize_transcript(transcript)
-        # Replace these lines with the actual code for transcription and summarization.
-
-        transcript = "This is a placeholder transcript"
-        summary = "This is a placeholder summary"
-
-        return jsonify({
-            'transcript': transcript,
-            'summary': summary
-        }), 200
+        result = summarize_local_podcast(podcast_url, test_run)
+        return jsonify(result), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
